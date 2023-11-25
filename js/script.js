0551d51b6acbb405b5912f51368e74364f1a2d17
ivxng <gonzalez.martino.ivan@gmail.com>
2023-11-25 20:38:23 +0100

    v1.1.1

--- js/script.js
+++ js/script.js
@@ -86,26 +86,6 @@ function handleApiButtonClick() {
 
 apiButton.addEventListener("click", handleApiButtonClick);
 
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('mobile-menu');
-    const menu = document.querySelector('.menu');
-  
-    menuToggle.addEventListener('click', function () {
-      menu.classList.toggle('active');
-    });
-  
-    document.addEventListener('click', function (e) {
-      const target = e.target;
-  
-      if (!menu.contains(target) && !menuToggle.contains(target)) {
-        menu.classList.remove('active');
-      }
-    });
-  
-    // Cerrar el menú al hacer clic en un elemento del menú
-    menu.addEventListener('click', function () {
-      menu.classList.remove('active');
-    });
-});
+
 
 
